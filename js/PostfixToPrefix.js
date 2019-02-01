04857f933a1acc681091fc263c4e255b22b5f056
VaibhavDomadia <44730103+VaibhavDomadia@users.noreply.github.com>
2019-02-01 20:25:37 +0530

    Add files via upload

---
+++ js/PostfixToPrefix.js
@@ -0,0 +1,88 @@
+let preCount = 0;
+
+function convertPostExpToPre() {
+    let table = document.getElementById("display");
+
+    for(let i=0 ; i<preCount ; i++) {
+        table.deleteRow(1);
+    }
+
+    let s = document.getElementById("getInput").value;
+    let out = document.getElementById("output");
+    if(s.length != 0) {
+        out.innerHTML = ptop(table,s);
+    }
+    preCount = s.length;
+}
+
+
+class Stack {
+    constructor(n) {
+        this.tos = -1;
+        this.N = n;
+        this.a = new Array(n);
+    }
+
+    push(val) {
+        if(this.tos == this.N-1) {
+            console.log("Stack Full");
+        }
+        else {
+            this.a[++this.tos] = val;
+        }
+    }
+
+    pop() {
+        if(this.tos == -1) {
+            console.log("Stack Empty");
+        }
+        else {
+            return this.a[this.tos--];
+        }
+    }
+
+    peek() {
+        return this.a[this.tos];
+    }
+
+    display() {
+        let sdis = "";
+        for(let i=0 ; i<=this.tos ; i++) {
+            sdis += this.a[i] + "    ";
+        }
+        return sdis;
+    }
+}
+
+
+
+let ptop = function(table,s) {
+    let st = new Stack(s.length);
+    for(let i=0 ; i<s.length ; i++) {
+        let row = table.insertRow(i+1);
+
+        if(isCharOrNum(s[i])) {
+            st.push(s[i]);
+        }
+        else {
+            let x1 = st.pop();
+            let x2 = st.pop();
+            st.push(s[i] + x2 + x1);
+        }
+
+        row.insertCell(0).innerHTML = s[i];
+        row.insertCell(1).innerHTML = st.display();
+    }
+    return st.pop();
+}
+
+let isCharOrNum = function(x) {
+    if(x >= 'a' && x <= 'z')
+        return true;
+    else if(x >= 'A' && x <= 'Z')
+        return true;
+    else if(x >= '0' && x <= '9')
+        return true;
+    else
+        return false;
+}
